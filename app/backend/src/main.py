2d07eb63e6d82d7a867839a035ab1d05b3012cbe
ashadrin <ashadrin701@gmail.com>
2025-02-06 10:15:17 +0300

    Test alor_api_test

--- app/backend/src/main.py
+++ app/backend/src/main.py
@@ -1,7 +1,10 @@
+from app.alor_api_test import AlorApiTest
 from app.option_app import OptionApp
 
 
 def main():
+    # alor_api_test = AlorApiTest()
+    # alor_api_test.run()
     option_app = OptionApp()
     option_app.start()
 
