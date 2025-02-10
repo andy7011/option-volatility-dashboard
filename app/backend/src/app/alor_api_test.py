4ce654b59a985e17d20a1a330912ce9ad410aef8
Andy701 <ashadrin701@gmail.com>
2025-02-10 23:02:56 +0300

    "Эксперимент AlorApiTest()" 10.02.2025

--- app/backend/src/app/alor_api_test.py
+++ app/backend/src/app/alor_api_test.py
@@ -1,15 +1,18 @@
 from infrastructure import env_utils
 from infrastructure.alor_api import AlorApi
-from infrastructure.moex_api import get_futures_series
 from implied_volatility import get_iv_for_option_price
 from supported_base_asset import MAP
 from moex_api import get_option_series
 from moex_api import get_option_list_by_series
 from moex_api import get_security_description
 from central_strike import get_list_of_strikes
+from datetime import datetime, timedelta  # Дата и время, временной интервал
+from pytz import timezone, utc  # Работаем с временнОй зоной и UTC
 import time
 
+
 class AlorApiTest:
+    tz_msk = timezone('Europe/Moscow')  # Время UTC будем приводить к московскому времени
 
     def __init__(self):
         print('AlorApiTest')
@@ -20,6 +23,46 @@ class AlorApiTest:
         self._test_subscribe_to_quotes()
         self._alorApi.run_async_connection(False)
 
+    def msk_datetime_to_utc_timestamp(self, dt) -> int:
+        """Перевод московского времени в кол-во секунд, прошедших с 01.01.1970 00:00 UTC
+
+        :param datetime dt: Московское время
+        :return: Кол-во секунд, прошедших с 01.01.1970 00:00 UTC
+        """
+        dt_msk = self.tz_msk.localize(dt)  # Заданное время ставим в зону МСК
+        return int(dt_msk.timestamp())  # Переводим в кол-во секунд, прошедших с 01.01.1970 в UTC
+
+    def utc_timestamp_to_msk_datetime(self, seconds) -> datetime:
+        """Перевод кол-ва секунд, прошедших с 01.01.1970 00:00 UTC в московское время
+
+        :param int seconds: Кол-во секунд, прошедших с 01.01.1970 00:00 UTC
+        :return: Московское время без временнОй зоны
+        """
+        dt_utc = datetime.utcfromtimestamp(seconds)  # Переводим кол-во секунд, прошедших с 01.01.1970 в UTC
+        return self.utc_to_msk_datetime(dt_utc)  # Переводим время из UTC в московское
+
+    def msk_to_utc_datetime(self, dt, tzinfo=False) -> datetime:
+        """Перевод времени из московского в UTC
+
+        :param datetime dt: Московское время
+        :param bool tzinfo: Отображать временнУю зону
+        :return: Время UTC
+        """
+        dt_msk = self.tz_msk.localize(dt)  # Задаем временнУю зону МСК
+        dt_utc = dt_msk.astimezone(utc)  # Переводим в UTC
+        return dt_utc if tzinfo else dt_utc.replace(tzinfo=None)
+
+    def utc_to_msk_datetime(self, dt, tzinfo=False) -> datetime:
+        """Перевод времени из UTC в московское
+
+        :param datetime dt: Время UTC
+        :param bool tzinfo: Отображать временнУю зону
+        :return: Московское время
+        """
+        dt_utc = utc.localize(dt)  # Задаем временнУю зону UTC
+        dt_msk = dt_utc.astimezone(self.tz_msk)  # Переводим в МСК
+        return dt_msk if tzinfo else dt_msk.replace(tzinfo=None)
+
     def _test_subscribe_to_quotes(self):
         print('\n _test_subscribe_to_quotes')
         for ticker in MAP.keys():
@@ -29,7 +72,7 @@ class AlorApiTest:
             self._alorApi.subscribe_to_instrument(ticker, self._handle_option_instrument_event)
 
     def _handle_quotes_event(self, ticker, data):
-        # print(ticker, 'last_price:', data['last_price'], 'last_price_timestamp:', data['last_price_timestamp'], 'bid:', data['bid'], 'ask:', data['ask'])
+        # print(datetime.now(), ticker, 'last_price:', data['last_price'], 'last_price_timestamp:', data['last_price_timestamp'], 'bid:', data['bid'], 'ask:', data['ask'])
         if ticker in MAP.keys():
             base_asset_last_price = data['last_price']
             last_price_futures[ticker] = base_asset_last_price
@@ -39,18 +82,18 @@ class AlorApiTest:
             ask = data['ask']
             bid = data['bid']
             option = ticker
-            base_asset_last_price = last_price_futures['RIH5']
+            # base_asset_last_price = last_price_futures['RIH5']
             # if ask:
             #     ask_iv = get_iv_for_option_price(base_asset_last_price, option, ask)
             # if bid:
             #     bid_iv = get_iv_for_option_price(base_asset_last_price, option, bid)
             # print(ticker, 'last_price:', data['last_price'], 'last_price_timestamp:', data['last_price_timestamp'], 'bid:',
             #     data['bid'], 'bid_iv:', bid_iv, 'ask:', data['ask'], 'ask_iv:', ask_iv)
-            print(ticker, 'last_price:', data['last_price'], 'last_price_timestamp:', data['last_price_timestamp'],
+            print(datetime.now(), ticker, 'last_price:', data['last_price'], 'last_price_timestamp:', data['last_price_timestamp'],
                   'bid:', data['bid'], 'ask:', data['ask'])
 
     def _handle_option_instrument_event(self, ticker, data):
-        print(ticker, 'theorPrice:', data['theorPrice'], 'volatility:', data['volatility'])
+        print(datetime.now(), ticker, 'theorPrice:', data['theorPrice'], 'volatility:', data['volatility'])
 
 
 # Определяем список базовых активов
